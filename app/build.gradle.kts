41539da54e857e266e12e37ed0b2959e909d9d0a
u1145h <ullashroy@pm.me>
2026-08-07 03:16:17 +0530

    Bump to v1.1.0, ignore .vscode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.u1145h.kavitaandroid"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
